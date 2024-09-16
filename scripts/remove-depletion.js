52aa721dafd376f1092d40d311ac29a3bc927738
Iqbal Asyadad <asyadad.iqbal@gmail.com>
2024-09-16 11:36:04 +0800

    remove fg feature

--- scripts/remove-depletion.js
+++ scripts/remove-depletion.js
@@ -19,26 +19,6 @@ const pp_input_grid = new DataGridXL("pp-input-grid", {
     frozenRows: 0
 });
 
-const fg_input_grid = new DataGridXL("fg-input-grid", { 
-    data: DataGridXL.createEmptyData(20, 2),
-    columns: [
-        {title: "FG DEPTH"},
-        {title: "FG SG"}
-    ],
-    allowDeleteCols: false,
-    allowMoveCols: false,
-    allowInsertCols: false,
-    allowHideCols: false,
-    allowHideRows: false,
-    allowMoveRows: false,
-    colHeaderHeight: 20,
-    colHeaderWidth: 50,
-    colHeaderLabelType: "numbers",
-    colHeaderLabelAlign: "center",
-    colAlign: "right",
-    rowHeight: 20,
-    frozenRows: 0
-});
 
 const pp_shale_output_grid = new DataGridXL("pp-shale-output-grid", { 
     data: DataGridXL.createEmptyData(20, 2),
@@ -61,26 +41,6 @@ const pp_shale_output_grid = new DataGridXL("pp-shale-output-grid", {
     frozenRows: 0
 });
 
-const fg_shale_output_grid = new DataGridXL("fg-shale-output-grid", { 
-    data: DataGridXL.createEmptyData(20, 2),
-    columns: [
-        {title: "FG DEPTH"},
-        {title: "FG SG"}
-    ],
-    allowDeleteCols: false,
-    allowMoveCols: false,
-    allowInsertCols: false,
-    allowHideCols: false,
-    allowHideRows: false,
-    allowMoveRows: false,
-    colHeaderHeight: 20,
-    colHeaderWidth: 50,
-    colHeaderLabelType: "numbers",
-    colHeaderLabelAlign: "center",
-    colAlign: "right",
-    rowHeight: 20,
-    frozenRows: 0
-});
 
 const pp_sand_output_grid = new DataGridXL("pp-sand-output-grid", { 
     data: DataGridXL.createEmptyData(20, 2),
@@ -103,26 +63,6 @@ const pp_sand_output_grid = new DataGridXL("pp-sand-output-grid", {
     frozenRows: 0
 });
 
-const fg_sand_output_grid = new DataGridXL("fg-sand-output-grid", { 
-    data: DataGridXL.createEmptyData(20, 2),
-    columns: [
-        {title: "FG DEPTH"},
-        {title: "FG SG"}
-    ],
-    allowDeleteCols: false,
-    allowMoveCols: false,
-    allowInsertCols: false,
-    allowHideCols: false,
-    allowHideRows: false,
-    allowMoveRows: false,
-    colHeaderHeight: 20,
-    colHeaderWidth: 50,
-    colHeaderLabelType: "numbers",
-    colHeaderLabelAlign: "center",
-    colAlign: "right",
-    rowHeight: 20,
-    frozenRows: 0
-});
 
 // PLOTLY
 var trace1 = {
@@ -204,20 +144,23 @@ var layout = {
 
 Plotly.newPlot('plotly-plot', data, layout);
 
-function get_col_ab(table_grid) {
+// Function to retrieve all data from the grid
+function get_col_ab(talbleGrid) {
+    const allData = talbleGrid.getData(); // Get all data
+    const nrow = allData.length;
+
     var col_a = [];
     var col_b = [];
-    const nrow = table_grid._cellStore.length;
-    line_list = table_grid.getCellRangeData([{x: 0, y:0}, {x: 1, y: nrow-1}]);
-    for (let i=0; i<line_list.length; i++) {
-        if (line_list[i][0]!==null && line_list[i][1]!==null) {
-            col_a.push(parseFloat(line_list[i][0]))
-            col_b.push(parseFloat(line_list[i][1]))
+    for (let i=0; i<nrow; i++) {
+        if (allData[i][0] !== null && allData[i][1] !== null) {
+            col_a.push(parseFloat(allData[i][0]));
+            col_b.push(parseFloat(allData[i][1]));
         } else {
             continue;
         }
     }
-    return [col_a, col_b];
+    console.log(col_a, col_b);
+    return[col_a, col_b];
 }
 
 function remove_depletion(depths, vals, cutoff) {
@@ -308,61 +251,7 @@ function run_pp() {
 
 }
 
-function run_fg() {
-    const [fg_input_depths, fg_input_vals] =  get_col_ab(fg_input_grid);
-    let [fg_shale_depths, fg_shale_vals, fg_sand_depths, fg_sand_vals] = remove_depletion(fg_input_depths, fg_input_vals, 0.025);
-    let fg_shale_list = col_to_row(fg_shale_depths, fg_shale_vals);
-    let fg_sand_list = col_to_row(fg_sand_depths, fg_sand_vals);
-
-    const fg_shale_output_grid = new DataGridXL("fg-shale-output-grid", { 
-        data: fg_shale_list,
-        columns: [
-            {title: "FG DEPTH"},
-            {title: "FG SG"}
-        ],
-        allowDeleteCols: false,
-        allowMoveCols: false,
-        allowInsertCols: false,
-        allowHideCols: false,
-        allowHideRows: false,
-        allowMoveRows: false,
-        colHeaderHeight: 20,
-        colHeaderWidth: 50,
-        colHeaderLabelType: "numbers",
-        colHeaderLabelAlign: "center",
-        colAlign: "right",
-        rowHeight: 20,
-        frozenRows: 0
-    });
-
-    const fg_sand_output_grid = new DataGridXL("fg-sand-output-grid", { 
-        data: fg_sand_list,
-        columns: [
-            {title: "FG DEPTH"},
-            {title: "FG SG"}
-        ],
-        allowDeleteCols: false,
-        allowMoveCols: false,
-        allowInsertCols: false,
-        allowHideCols: false,
-        allowHideRows: false,
-        allowMoveRows: false,
-        colHeaderHeight: 20,
-        colHeaderWidth: 50,
-        colHeaderLabelType: "numbers",
-        colHeaderLabelAlign: "center",
-        colAlign: "right",
-        rowHeight: 20,
-        frozenRows: 0
-    });
-}
-
 const submitPPButton = document.getElementById("submit-pp-btn");
 submitPPButton.onclick = function(){
     run_pp();
-}
-
-const submitFGButton = document.getElementById("submit-fg-btn");
-submitFGButton.onclick = function(){
-    run_fg();
 }
